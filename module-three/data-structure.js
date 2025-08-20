22b054d4914f65288f2dd461f7e2f698c7e0ecd9
Cyprian Omenuko <cykeromens@gmail.com>
2025-08-20 20:14:29 +0000

    refactor: define data structure

---
+++ module-three/data-structure.js
@@ -0,0 +1,3 @@
+// Data Structures
+// Data structures are ways of organizing data so that it can be used efficiently.
+// They are used to store and organize data in a way that makes it easy to access and manipulate.

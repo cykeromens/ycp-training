69105d50f5b90ecd9a007612c404177a6344ef31
Cyprian Omenuko <cykeromens@gmail.com>
2025-08-20 21:41:55 +0000

    add: array data structure

--- module-three/data-structure.js
+++ module-three/data-structure.js
@@ -1,3 +1,62 @@
 // Data Structures
 // Data structures are ways of organizing data so that it can be used efficiently.
 // They are used to store and organize data in a way that makes it easy to access and manipulate.
+
+// Actions on data structures
+// 1. Create
+// 2. Transverse (Read)
+// 3. Insert
+// 4. Delete
+// 5. Search **
+// 6. Sort **
+
+
+// Types of data structures
+// 1. Primitive data structures
+// 2. Non-primitive data structures
+// 3. Abstract data structures
+
+//. Primitive data structures
+// String, Number, Boolean, Undefined, Null, Symbol, BigInt
+
+// Non-primitive data structures (Physical data structures)
+// Object, Array, Function, Date, Classes
+
+// Abstract data structures (Complex data structures))
+// Stack, Queue, Linked List, Tree, Graph, Hash Table, Heap, Trie, Set, Map
+
+
+
+// Stack
+// LIFO - Last In First Out
+// Push, Pop, Peek, isEmpty, size
+
+// Queue
+// FIFO - First In First Out
+// Enqueue, Dequeue, Peek, isEmpty, size
+
+
+
+// Big O Notation
+// Big O notation is a way of describing the performance of an algorithm.
+// It is used to describe the upper bound of the running time of an algorithm.
+// It is used to describe the worst case scenario of an algorithm.
+// It is used to describe the best case scenario of an algorithm.
+
+// Big 0 cases
+// O(1) - Constant time
+// O(n) - Linear time
+// O(n^2) - Quadratic time
+// O(log n) - Logarithmic time
+// O(n log n) - Linearithmic time
+
+// Omega Notation
+// Omega notation is a way of describing the performance of an algorithm.
+// It is used to describe the lower bound of the running time of an algorithm.
+// It is used to describe the best case scenario of an algorithm.
+
+// Delta Notation
+// Delta notation is a way of describing the performance of an algorithm.
+// It is used to describe the average case scenario of an algorithm.
+// It is used to describe the average case scenario of an algorithm.
+// It is used to describe the average case scenario of an algorithm.
